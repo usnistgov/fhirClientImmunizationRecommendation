1e90e567a01c2aee3432bb88ba0ab92fab04ca65
Andrew McCaffrey <andrew.mccaffrey@nist.gov>
2017-11-17 12:10:25 -0500

    README.md added

--- src/main/java/gov/nist/fhir/client/ir/TestRunnerServiceFhirImpl.java
+++ src/main/java/gov/nist/fhir/client/ir/TestRunnerServiceFhirImpl.java
@@ -272,10 +272,10 @@ public class TestRunnerServiceFhirImpl implements TestRunnerService {
        
            //TestRunnerService test = new TestRunnerServiceFhirImpl("http://localhost:8084/fhirAdapter/fhir/Parameters/$cds-forecast");
      //  TestRunnerService test = new TestRunnerServiceFhirImpl("http://localhost:9080/fhirAdapter/fhir/Parameters/$cds-forecast");
-   // TestRunnerService test = new TestRunnerServiceFhirImpl("http://hit-dev.nist.gov:11080/fhirAdapter/fhir/Parameters/$cds-forecast");
+    TestRunnerService test = new TestRunnerServiceFhirImpl("http://hit-dev.nist.gov:11080/fhirAdapter/fhir/Parameters/$cds-forecast");
 
    
-   TestRunnerService test = new TestRunnerServiceFhirImpl("http://localhost:9080/fhirAdapter/fhir/Parameters/$cds-forecast");
+   //TestRunnerService test = new TestRunnerServiceFhirImpl("http://localhost:9080/fhirAdapter/fhir/Parameters/$cds-forecast");
    
 //TestRunnerService test = new TestRunnerServiceFhirImpl();
         SoftwareConfig config = new SoftwareConfig();
